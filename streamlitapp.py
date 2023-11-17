44ba316c8292852c87505829f3d9c140749e5f17
pghonge <150998084+pghonge@users.noreply.github.com>
2023-11-17 15:08:55 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -39,4 +39,5 @@ streamlit.dataframe(my_data_rows)
 fruit_added = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thank you for adding ', fruit_added)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"jackfruit")
-#streamlit.text(fruityvice_response.json())
+
+mycur.execute(insert into PC_RIVERY_DB.PUBLIC.fruit_load_list values ('from streamlittest'));
