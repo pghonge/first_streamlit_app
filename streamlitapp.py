649a47a0e2db58d02f92751e1a4de719d61be1cf
pghonge <150998084+pghonge@users.noreply.github.com>
2023-11-17 15:50:20 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -23,6 +23,11 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
+  
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * from fruit_load_list")
+    return my_cur.fetchall()
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -42,10 +47,7 @@ except URLError as e:
   streamlit.error()
 
 
-def get_fruit_load_list():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("SELECT * from fruit_load_list")
-    return my_cur.fetchall()
+
 
 
 
