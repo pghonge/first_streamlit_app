c25c38832cc1537e4a0ea8b91b9d47b768d4019e
pghonge <150998084+pghonge@users.noreply.github.com>
2023-11-17 13:06:38 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -9,7 +9,7 @@ streamlit.text('🥑🍞 avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
