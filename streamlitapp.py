26dedd12e18810b58cb2f868950fc04fdb2062de
pghonge <150998084+pghonge@users.noreply.github.com>
2023-11-17 15:29:22 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -30,21 +30,4 @@ try:
     #streamlit.text(fruityvice_response.json())
 except URLError as e:
   streamlit.error()
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 
-streamlit.stop()
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
-
-fruit_added = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('Thank you for adding ', fruit_added)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"jackfruit")
-
-my_cur.execute("insert into fruit_load_list values ('from streamlittest')");
